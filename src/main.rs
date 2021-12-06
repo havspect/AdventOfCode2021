212b775733702737b9fd026ce9d1f2498fef9744
havspect <54222112+havspect@users.noreply.github.com>
2021-12-06 16:45:42 +0000

    Function to open and read files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
   println!("Hello World!")
 }
+
+fn read_file_per_line(file_name: &str) -> Result<Vec<String>, Box<dyn Error>> {
+  let input: String = fs::read_to_string(file_name)?.parse()?;
+  let vec: Vec<String> = input.split("\n").map(|x| x.to_string()).collect();
+  return Ok(vec)
+}
